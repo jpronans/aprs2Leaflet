41ba50161bf6c52f6f1fa55d7fa202a58d2f4f0c
John Ronan <jpronans@users.noreply.github.com>
2017-03-28 09:18:16 +0100

    * Updated to only record moving stations

--- src/aprs2Leaflet.py
+++ src/aprs2Leaflet.py
@@ -2,11 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from ConfigParser import SafeConfigParser
-#import time
+import time
 import aprslib
 import logging
 import os
-#import locale
+#import utf8
 
 parser = SafeConfigParser()
 parser.read('config.ini')
@@ -16,62 +16,62 @@ else:
     logging.basicConfig(level=logging.INFO)
 
 
-# I don't care about aprsdroid or anything that doesn't have a 'speed' field.
 def callback(packet):
-    if ('latitude' in packet and
-        'longitude' in packet and
-        'speed' in packet and
-        packet['to'] != 'APDR13'):
-        f = open("aprstations.js", "a+")
+    ignorePacket = 1
 
-        # Code taken from:
-        # http://stackoverflow.com/questions/1877999/delete-final-line-in-file-via-python
-        # Move the pointer (similar to a cursor in a text editor) to the end of the file.
-        f.seek(0, os.SEEK_END)
+    # Station needs to be moving and not an APRS Droid App. RF Stations only
+    if 'latitude' in packet and 'longitude' in packet and 'speed' in packet and packet['to'] != 'APDR13':
+        # Packet has the speed attribute, but is it greater than 0?
+        if float(packet['speed']) > 0.0:
+            ignorePacket = 0; 
+            logging.debug("Speed is > 0, record packet") 
+            f = open("/var/www/ireland.aprs2.net/aprstations.js","a+")
+            #Move the pointer (similar to a cursor in a text editor) to the end of the file. 
+            f.seek(0, os.SEEK_END)
 
-        # This code means the following code skips the very last character in the file -
-        # i.e. in the case the last line is null we delete the last line
-        # and the penultimate one
-        pos = f.tell() - 1
+            #This code means the following code skips the very last character in the file - 
+            #i.e. in the case the last line is null we delete the last line 
+            #and the penultimate one
+            pos = f.tell() - 1
 
-        # Read each character in the file one at a time from the penultimate 
-        # character going backwards, searching for a newline character
-        # If we find a new line, exit the search
-        while pos > 0 and f.read(1) != "\n":
-                pos -= 1
-                f.seek(pos, os.SEEK_SET)
+	    #Read each character in the file one at a time from the penultimate 
+	    #character going backwards, searching for a newline character
+	    #If we find a new line, exit the search
+	    while pos > 0 and f.read(1) != "\n":
+		pos -= 1
+		f.seek(pos, os.SEEK_SET)
 
-        # So long as we're not at the start of the file,
-        # delete all the characters ahead of this position
-        if pos > 0:
-            f.seek(pos, os.SEEK_SET)
-            f.truncate()
+	    #So long as we're not at the start of the file, delete all the characters ahead of this position
+	    if pos > 0:
+		f.seek(pos, os.SEEK_SET)
+		f.truncate()
+	        try:
+	            outStr = "\n["+str(packet['latitude'])+", "+str(packet['longitude'])+", 1], <!-- "+str(packet['raw'])+" -->\n"
+		    f.write(outStr)
 
-        # Generate the required format
-        outStr = "\n[%.5f, %.5f, 1]    <!-- %s -->\n]];\n" % (float(packet['latitude']), float(packet['longitude']), repr(packet['raw']))
+	        except:
+	            outStr = "Exception in string output, probably UTF8"
+	            logging.debug(outStr)
+                f.write("];\n")
+	        f.close()
+	        
+                logging.info(outStr)
+	        f = open("aprslogged.txt", "a")
+	        try:
+	            f.write(packet['raw']+"\n")
+	        except:
+	            logging.debug("Exception in string output, probably UTF8")
+	        f.close()
 
-        # Write it
-        try:
-            f.write(outStr)
-
-        except:
-            outStr = "Could not write log file, check it.\n"
-            logging.debug(outStr)
-
-        # Close it
-        f.close()
-
-        logging.debug(outStr)
-    else:
-        # Record what we ignored
+    if (ignorePacket == 1):
         f = open("aprsignored.txt", "a")
-        outStr = "Ignoring: "+repr(packet['raw'])+"\n"
-        try:
-            f.write(outStr)
-        except:
-            logging.debug("Could not write 'ignored' file, check it.\n")
-
+	outStr = packet['raw']+"\n"
+	try:
+	    f.write(outStr)
+	except:
+	    logging.debug("Exception in string output, probably UTF8")
         f.close()
+	#logging.debug(outStr)
 
 # Prepare the APRS connection
 aprs = aprslib.IS(parser.get('aprs', 'callsign'),
@@ -82,8 +82,14 @@ aprs = aprslib.IS(parser.get('aprs', 'callsign'),
 # Listen for specific packet
 aprs.set_filter(parser.get('aprs', 'filter'))
 
-# Open the APRS connection to the server
-aprs.connect()
+while True:
+    # Open the APRS connection to the server
+    aprs.connect()
 
-# Set a callback for when a packet is received
-aprs.consumer(callback)
+    # Set a callback for when a packet is received
+    try:
+        aprs.consumer(callback)
+    except:
+        logging.info("Exception in aprs.consumer, restarting connection to APRS server")
+
+
